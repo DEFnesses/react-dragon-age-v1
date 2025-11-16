755c1ca0a314453e4d0e2e600dfdeab667a57db7
Dmitry Gazdik <dimagazdik@gmail.com>
2025-11-16 23:53:34 +0700

    #1 Styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "../src/assets/img/icons/logo.svg";
+import warriorIcon from "../src/assets/img/icons/warrior/warrior.svg";
+import warriorSpIcon from "../src/assets/img/icons/warrior/warrior-sp1.svg";
+import hmIcon from "../src/assets/img/icons/race/hm.svg";
+import p1 from "../src/assets/img/portraits/1.jpg";
+import checkIcon from "../src/assets/img/icons/check.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div>
+          <img src={logo} alt="logo"/>
+          <div>
+            <h3>Dragon Age party maker</h3>
+            <p>Подбери идеальную команду</p>
+          </div>
+        </div>
       </header>
+<h2 className="text-3xl font-bold underline">
+      Hello world!
+    </h2>
+      <div>
+        <div>
+          <h1>Все персонажи</h1>
+          <input placeholder="Поиск..."></input>
+        </div>
+
+        <div>
+          <div>
+            <div>
+              <img src={warriorIcon} alt="warriorIcon" />
+              <img src={warriorSpIcon} alt="warriorSpIcon" />
+              <img src={hmIcon} alt="hmIcon" />
+            </div>
+
+            <img src={p1} alt="p1" />
+            <h5>Блэкволл</h5>
+
+            <div>
+              <img src={checkIcon} alt="checkIcon" />
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
