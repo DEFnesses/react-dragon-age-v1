1964540b46a773dae093075e6d6aa9c986429bba
Dmitry Gazdik <dimagazdik@gmail.com>
2025-11-19 21:55:42 +0700

    #1 Home Layout

--- src/App.js
+++ src/App.js
@@ -6,45 +6,44 @@ import hmIcon from "../src/assets/img/icons/race/hm.svg";
 import p1 from "../src/assets/img/portraits/1.jpg";
 import checkIcon from "../src/assets/img/icons/check.svg";
 
-
 function App() {
   return (
     <div className="wrapper">
-      <header className="flex justify-between items-center pl-5">
+      <header className="flex justify-between items-center pl-[45px] pb-[45px]">
         <div className="flex">
-          <img className="mr-[15px]" src={logo} alt="logo"/>
+          <img src={logo} alt="logo" />
           <div>
-            <h3 className="uppercase">Dragon Age party maker</h3>
+            <h3 className="uppercase text-[20px] font-bold">
+              Dragon Age party maker
+            </h3>
             <p className="opacity-50">Подбери идеальную команду</p>
           </div>
         </div>
       </header>
-<h2 className="text-3xl font-bold underline">
-      Hello world!
-    </h2>
-      <div>
-        <div>
-          <h1>Все персонажи</h1>
-          <input placeholder="Поиск..."></input>
+
+      <div className="pl-[45px] pr-[45px]">
+        <div className="flex justify-between items-center mb-[40px]">
+          <h1 className="text-[32px] font-bold">Все персонажи</h1>
+          <input className="search" placeholder="Поиск..."></input>
         </div>
 
-        <div>
-          <div>
+        <div className="flex ">
+          <div className="flex">
             <div>
               <img src={warriorIcon} alt="warriorIcon" />
               <img src={warriorSpIcon} alt="warriorSpIcon" />
               <img src={hmIcon} alt="hmIcon" />
             </div>
-
-            <img src={p1} alt="p1" />
-            <h5>Блэкволл</h5>
+            <div className="flex flex-col items-center">
+              <img src={p1} alt="p1" />
+              <h5>Блэкволл</h5>
+            </div>
 
             <div>
               <img src={checkIcon} alt="checkIcon" />
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
